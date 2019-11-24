fb9a9519505dc2eec28e8e0738c81f93a6e03c75
Igor <Igor@example.com>
2019-11-24 19:20:17 +0300

    Add Lesson_16

---
+++ Lesson_16/main.js
@@ -0,0 +1,62 @@
+var button = document.getElementsByTagName('button')[0];
+var tabs = document.getElementsByClassName('tabs')[0];
+var info = document.getElementsByClassName('info')[0];
+info.setAttribute('style', 'visibility: hidden');
+var responseUsersList = [];
+
+button.onclick = function () {
+    var xhr = new XMLHttpRequest();
+    if (localStorage.getItem('userData')) {
+        responseUsersList = JSON.parse(localStorage.getItem('userData'))
+        showUsersList(responseUsersList);
+    } else {
+        xhr.open('GET', 'https://reqres.in/api/users?page=2');
+        xhr.onload = function () {
+            var statusType = +String(this.status)[0];
+            if (statusType === 2) {
+                responseUsersList = JSON.parse(this.response).data;
+                localStorage.setItem('userData', JSON.stringify(responseUsersList));
+                showUsersList(responseUsersList);
+            } else {
+                showError(this.status);
+            }
+        }
+        xhr.onerror = function () {
+            showError(this.status);
+        };
+        xhr.send();
+    }
+};
+
+function showUsersList(users) {
+    tabs.innerHTML = "";
+    for (var i = 0; i < users.length; i++) {
+        tabs.insertAdjacentHTML('beforeEnd', '<div class="tab" data-userId =' + i + ' >User' + (i + 1) + '</div>')
+    }
+    document.getElementsByClassName('tab')[0].classList.add('active')
+    showUserInfo(0);
+}
+
+function showUserInfo(userId) {
+    info.innerHTML = '';
+    info.setAttribute('style', 'visibility: show');
+    info.insertAdjacentHTML("beforeend", '<div class="photo"><img src="' + responseUsersList[userId]['avatar'] + '"></div>')
+    info.insertAdjacentHTML('beforeend', '<div class="userInfo">' +
+        '<p>First name: ' + responseUsersList[userId]['first_name'] + '</p>' +
+        '<p>Last name: ' + responseUsersList[userId]['last_name'] + '</p>' +
+        '<p>email: ' + responseUsersList[userId]['email'] + '</p></div>')
+}
+
+function showError(status) {
+    info.setAttribute('style', 'visibility: show');
+    info.insertAdjacentHTML('beforeend', '<div class="userInfo"> Ошибка загрузки данных (Error:' + status + ')</div>')
+}
+
+tabs.onclick = function (event) {
+    document.querySelectorAll('.active').forEach(function (tab) {
+        tab.classList.remove('active')
+    });
+    var target = event.target;
+    target.classList.add('active')
+    showUserInfo(target.getAttribute('data-userId'))
+};
